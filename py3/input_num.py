8e23cf5adec12d6e3cbf27b486d7ff1e07516341
zhangjunfeng <dodowolf-zjf@163.com>
2020-09-26 22:49:35 +0800

    add file py3/input_num.py

---
+++ py3/input_num.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+'''
+test input
+'''
+
+name = input("your name: ")
+age = input("your age: ")
+after = int(age) + 10
+
+print("your name is: ", name)
+print("after ten years,you are ", after)
